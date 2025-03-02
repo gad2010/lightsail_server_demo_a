33159025021adc9dc30f5e3e6f77c95e57476d41
gad ndayishimiye <r1gady@yahoo.fr>
2025-03-01 21:03:33 -0700

    changes

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_lightsail_instance" "gitlab_test" {
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
-  key_pair_name     = "dev-key"
+  # key_pair_name     = "dev-key"
   tags = {
     Name = "Lightsail-test"
     Team = "cloudops"
