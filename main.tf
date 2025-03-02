366114a01f4dde6d8b8fc329016468b73a8c9f54
gad ndayishimiye <r1gady@yahoo.fr>
2025-03-01 20:32:10 -0700

    adding main and provider files

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Create a new GitLab Lightsail Instance
+resource "aws_lightsail_instance" "gitlab_test" {
+  name              = "custom_gitlab"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  key_pair_name     = "dev-key"
+  tags = {
+    Name = "Lightsail-test"
+    Team = "cloudops"
+    env = "dev"
+    Created_by= "Terraform"
+  }
+}
