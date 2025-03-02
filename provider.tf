cdd5e9088e0f26afc17b68f9b6e4efc6833ac1da
gad ndayishimiye <r1gady@yahoo.fr>
2025-03-01 21:01:48 -0700

    changes

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = "default"
+  region  = "us-east-1"  # The AWS region to interact with (e.g., us-east-1)
+  #profile = "default"    # The AWS CLI profile to use for authentication (e.g., "default")
 }
